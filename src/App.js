1209a03452f4dabea5ae59bb6527f09dfd173714
Flávio <florentino.flavio@hotmail.com>
2019-11-06 20:02:07 -0300

    Added Layout component on App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
     <div>
+      <Layout>
+        <p>Initial boilerplate</p>
+      </Layout>
     </div>
   );
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,16 @@
-import React from 'React';
+import React from 'react';
+
+import Aux from '../../hoc/Wrapper';
 
 const layout = (props) => (
+   <Aux>
     <div>
         HTML CONTENT
     </div>
     <main>
         {props.children}
     </main>
+   </Aux>
 );
+
+export default layout;
