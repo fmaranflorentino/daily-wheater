08df3f7a24192230d45e5d92c7f12830c953e3a9
Flávio <florentino.flavio@hotmail.com>
2019-11-07 17:40:32 -0300

    feat(App): Added WheaterStatus container

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Wrapper from './hoc/Wrapper';
 import Layout from './components/Layout/Layout';
 
+import WheaterStatus from './containers/WheaterStatus/WheaterStatus';
+
 function App() {
   return (
     <Wrapper>
       <Layout>
-        <p>Initial boilerplate</p>
+        <WheaterStatus />
       </Layout>
     </Wrapper>
   );
