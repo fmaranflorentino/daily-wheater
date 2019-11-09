835578cc18720112048a2121144dba0f36fae715
Flávio <florentino.flavioweb@gmail.com>
2019-11-09 14:00:28 -0300

    refactor(containers) Changed Wheater container

--- src/containers/Wheater/Wheater.js
+++ src/containers/Wheater/Wheater.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Wrapper from '../../hoc/Wrapper';
+import Modal from './../../components/UI/Modal/Modal';
+
 
 class Wheater extends Component {
+  state = {
+    wheater: [],
+  }
+
+  componentWillMount() {
+    if (navigator.geolocation && navigator.geolocation.getCurrentPosition) {
+      navigator.geolocation.getCurrentPosition(this.getposition);
+    }
+  }
+
+  getposition = (position) => {
+    const latitude = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    const params = {
+      lat: latitude,
+      lon,
+      APPID: '3cc15f29b5e1a6baec11ab23768ab424',
+    };
+
+    axios.get(`http://api.openweathermap.org/data/2.5/weather`, { params })
+    .then(res => {
+      const wheater = res.data;
+      this.setState({ wheater });
+    })
+  }
+
   render() {
+    if (this.state && this.state.wheater) {
+      const infos = this.state.wheater;
+      return (
+        <Wrapper>
+          <p>
+            {infos.name}
+          </p>
+
+          {/* <Modal show={true} >
+               bctt
+                </Modal> */}
+        </Wrapper>
+      )
+    }
     return (
         <Wrapper>
             <div>wheater title</div>
