93e2674307531dc24aeb9ee60562736321287793
Flávio <florentino.flavioweb@gmail.com>
2019-11-09 14:21:57 -0300

    feat(Header) Added a title for the section

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header class="Header">
-            <p>header</p>
+            <p>Daily Wheater</p>
         </header>
     )
 }
