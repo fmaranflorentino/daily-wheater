204f584a01cace75804fd46b8c528dd8635f32f8
Flávio <florentino.flavioweb@gmail.com>
2019-11-10 22:34:22 -0300

    fix(loader) Fixed attr

--- src/components/UI/Loader/Loader.js
+++ src/components/UI/Loader/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Loader.css';
 
 const Loader = () => {
-    return <div class="Loader"></div>
+    return <div className="Loader"></div>
 }
 
 export default Loader;
